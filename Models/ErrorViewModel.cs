f3155d7da310adf44cb37c4f8fbb5f6130278836
Samantha Berg <sb11158@byu.edu>
2021-04-08 12:30:46 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FagElGamousExcavation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Pages/AdminPageModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FagElGamousExcavation.Areas.Identity.Pages
+{
+    public class AdminPageModel: PageModel
+    {
+    }
+}
